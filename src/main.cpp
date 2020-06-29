ea277cf8b5a344d011c7ab4bf4ffb0ca01bca709
Dejan <dejan@meznarc.si>
2020-06-29 20:10:59 +0200

    better ui

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ void waitForSerial();
 /**
  * Define PINS for ESC calibrations
  */
-const uint8_t motor_pins[MOTORS_NUM] = {15, 13,12,14};
+const uint8_t motor_pins[MOTORS_NUM] = {15, 13, 12, 14};
 
 
 /**
@@ -74,11 +74,20 @@ Servo motors[MOTORS_NUM];
 #endif
 
 
-
 void setup() {
     Serial.begin(500000);
     Serial.println(F("RESTART -----"));
     waitForSerial();
+
+    Serial.println("[INFO] Hello! Here is calibration procedure:");
+    Serial.println("[INFO] \t1. disconnect battery and propellers");
+    Serial.println("[INFO] ");
+    Serial.println("[INFO] \t3. ESC will make three beeps");
+    Serial.println("[INFO] \t4. Listen for one short beep - then press key");
+    Serial.println("[INFO] \t5. Listen for two beeps - then press key");
+    Serial.println("[INFO] Program will guide you through this procedure");
+    Serial.println("\n Press any key to continue. \n");
+
 
 
     //scroll thorugh all calibrations
@@ -95,46 +104,44 @@ void loop() {
 
 void calibration(int motor) {
 
-    Serial.println("Calibration of motor " + String(motor) + " will start.. \n");
+    Serial.println("[CALIBRATION] Calibration of motor " + String(motor) + " will start.. \n");
 
-    Serial.println("DISCONNNECT PROPELLERS!!! \n");
-    Serial.println("and your battery");
+    Serial.println("[CALIBRATION] \tnow you can disconnect battery and propellers");
     Serial.println("\n");
     Serial.println("press anything to continue");
     waitForSerial();
 
-
+    Serial.println("> motor attached");
     motors[motor].attach(motor_pins[motor]);
+    Serial.println("> generating 2ms pulse \n");
     motors[motor].writeMicroseconds(2000); //set to max throttle
 
 
-    Serial.println("connect flight battery");
-    Serial.println("You will hear 3 beeps, \n than you will hear one SHORT BEEP - after that one press key!");
+    Serial.println("[CALIBRATION] \t2. connect battery");
+    Serial.println("[CALIBRATION] You will hear 3 beeps, \n than you will hear one SHORT BEEP - after that one press key!");
     waitForSerial();
 
+    Serial.println("> generaating 1ms pulse");
     motors[motor].writeMicroseconds(1000); //set to max throttle
 
-    Serial.println("press something when you hear TWO BEEPS");
+    Serial.println("[CALIBRATION] press something when you hear TWO BEEPS");
     waitForSerial();
 
-    Serial.println("OK. now wait a bit!");
-    delay(1000);
-    Serial.println("waiting is done, you've successfuly calibrated motor" + String(motor));
+    Serial.println("> OK. now wait a bit!");
+    delay(5000);
+    Serial.println("[CALIBRATION] waiting is done, you've successfuly calibrated motor" + String(motor));
 
-
+    Serial.println("> detaching motor");
     motors[motor].detach();
 
-    Serial.println("Ending calibration \n ====================");
+    Serial.println("[CALIBRATION] Ending calibration \n ==================== \n \n");
 
 
 }
 
 void waitForSerial() {
     // Wait for serial communication
-    while (!Serial.available()) {
-        Serial.println("Press something");
-        delay(2000);
-    }
+    while (!Serial.available()) {}
 
     //read everything we recieved
     while (Serial.available()) {
